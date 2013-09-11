e2a1ecfd19418d2abaa26f43f960041e2a5b7c57
unknown <fisers@LCANB722.dds.dillards.net>
2013-09-11 08:29:26 -0500

    added scope to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name
-  def self.ordered
-  	order("last_name")
-  end
+  scope :ordered, order("last_name")
+  # def self.ordered
+  # 	order("last_name")
+  # end
 end
